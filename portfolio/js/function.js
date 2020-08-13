bd17fe5c22b94991892d03edbd71c86c0baa08e6
AyakaMuto <ayaka@Junji-no-MacBook-Air.local>
2020-08-13 15:56:32 +0900

    add protofolio
    Add protofolio web page.

---
+++ portfolio/js/function.js
@@ -0,0 +1,76 @@
+/************* radioBtn ***************/
+const initRadioBtns = () => {
+  let radios = document.querySelectorAll('.radio');
+  let labels = document.querySelectorAll('.label');
+  let ball = document.querySelector('.ball');
+  let prevRadio, prevLabel;
+  radios.forEach((radio, index) => {
+    radio.addEventListener('click', function (e) {
+      if (prevRadio) prevRadio.classList.toggle('active');
+      if (prevLabel) prevLabel.classList.toggle('active');
+      radio.classList.toggle('active');
+      prevRadio = radio;
+      labels[index].classList.toggle('active');
+      prevLabel = labels[index];
+      ball.className = `ball pos${index}`;
+    });
+  });
+
+  // Switch main picture on click
+  const ayakaBtn = document.getElementById('radio1');
+  const hidePics = () => $('.main-pic img').hide()
+
+  ayakaBtn.addEventListener('click', () => {
+    hidePics();
+    $('#main-picture1').show();
+  });
+
+  const mamaBtn = document.getElementById('radio2');
+  mamaBtn.addEventListener('click', () => {
+    hidePics();
+    $('#main-picture2').show();
+  });
+
+  const fashionBtn = document.getElementById('radio3');
+  fashionBtn.addEventListener('click', () => {
+    hidePics();
+    $('#main-picture3').show();
+  });
+
+  const snowboardBtn = document.getElementById('radio4');
+  snowboardBtn.addEventListener('click', () => {
+    hidePics();
+    $('#main-picture4').show();
+  });
+};
+
+/************* colorbox ***************/
+const initColorBox = () => {
+  $('.group1').colorbox({ rel: 'group1' });
+  $('.group2').colorbox({ rel: 'group2' });
+  /** 青線を消す **/
+  jQuery(document).on('cbox_closed', function () {
+    var reloadOnClosedSettings = jQuery.colorbox.reloadOnClosedSettings;
+    if (reloadOnClosedSettings.needReloadParent === true && reloadOnClosedSettings.reloadFunc && (typeof reloadOnClosedSettings.reloadFunc === "function")) {
+      reloadOnClosedSettings.reloadFunc();
+    }
+  });
+};
+
+/************* responsive humburger ***************/
+const initHumberger = () => {
+  $('.sp-menu').on('click', function () {
+    $(this).toggleClass('active');
+    $("#sp-nav").toggleClass('active');
+  })
+  // $('#nav a').on('click', function () {
+  //    $('#nav').toggleClass('active');
+  //     $(".menu").toggleClass('active');
+  // })
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  initHumberger();
+  initColorBox();
+  initRadioBtns();
+});
